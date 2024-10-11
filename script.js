96fb7d6c708ddce75ab96f74f507505aa8874061
Damilare Abogunrin <43679376+St-Thierry@users.noreply.github.com>
2024-10-11 14:17:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 
+document.addEventListener("DOMContentLoaded", function() {
+    const menuIcon = document.getElementById('menu-icon');
+    const dropdownMenu = document.getElementById('dropdown-menu');
+
+    menuIcon.addEventListener('click', function() {
+        // Toggle the active class to show/hide the dropdown
+        dropdownMenu.classList.toggle('active');
+        menuIcon.classList.toggle('active');
+    });
+});
